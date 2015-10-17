f0aa3eada624779220ed0f89545c2999b917a596
glee- <glee6848@yahoo.com>
2015-10-17 03:56:20 -0700

    Added several example puzzles, dedicated solve function

--- solver.py
+++ solver.py
@@ -1,5 +1,5 @@
 import pdb;
-board = [
+a = [
 [0,2,9,4,8,6,0,1,3],
 [1,3,0,7,9,0,4,6,0],
 [0,6,4,0,3,1,7,9,2],
@@ -9,6 +9,30 @@ board = [
 [9,0,7,8,2,0,6,3,4],
 [5,8,6,0,7,4,1,2,9],
 [2,4,3,1,6,0,8,5,7]
+]
+
+b = [
+[0,2,0,4,5,6,7,8,9],
+[4,5,7,0,8,0,2,3,6],
+[6,8,9,2,3,7,0,4,0],
+[0,0,5,3,6,2,9,7,4],
+[2,7,4,0,9,0,6,5,3],
+[3,9,6,5,7,4,8,0,0],
+[0,4,0,6,1,8,3,9,7],
+[7,6,1,0,4,0,5,2,8],
+[9,3,8,7,2,5,0,6,0]
+]
+
+c = [
+[2,9,5,7,0,0,8,6,0],
+[0,3,1,8,6,5,0,2,0],
+[8,0,6,0,0,0,0,0,0],
+[0,0,7,0,5,0,0,0,6],
+[0,0,0,3,8,7,0,0,0],
+[5,0,0,0,1,6,7,0,0],
+[0,0,0,5,0,0,1,0,9],
+[0,2,0,6,0,0,3,5,0],
+[0,5,4,0,0,8,6,7,2]
 ]
 
 # Board will init like this
@@ -101,6 +125,8 @@ def add_notes(section):
     for i in range(len(section)):
         if section[i] == 0 and len(missing) == 1:
             section[i] = missing[0]
+    #    elif len(missing) == 0:
+    #        print "We got the wrong guess"
         elif section[i] == 0:
             section[i] = missing
         elif type(section[i]) is list:
@@ -144,19 +170,19 @@ def iter(board):
     return board
 
 
-def solve(puzzle):
-    board = puzzle
+def solve(board):
+    out = board
+    for i in range(10):
+        out = iter(board)
+    if solved(out):
+        print "Puzzle Solved"
+        print "\n"
+    else:
+        print "Not quite yet"
+    printboard(out)
 
 #-------------------------------------------------
-#printboard(board)
-#print s1
-#print add_notes(get_area(board,5))
-#print add_notes([1,2,3,4,5,6,7,8,[5,9]])
-printboard(board)
+
 print "#######################"
-print add_notes(get_area(board,5))
+solve(a)
 print "#######################"
-printboard(iter(board))
-#printboard(sync_area(board,add_notes(get_area(board,5)),5))
-#printboard(sync_row(board, add_notes(get_row(board,6)), 6))
-#print sync_col(board, add_notes(get_col(board,6)), 6)[0]
