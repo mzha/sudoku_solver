65d9737821967191748e755ed69f79dfa45a2290
glee- <glee6848@yahoo.com>
2015-10-17 00:46:56 -0700

    Revised board init strategy

--- solver.py
+++ solver.py
@@ -3,6 +3,7 @@ board = [
 [1,3,0,7,9,0,4,6,0],
 [0,6,4,0,3,1,7,9,2],
 [3,9,1,6,4,7,2,0,5],
+[4,5,0,2,1,3,9,7,6],
 [0,7,2,9,0,8,0,4,1],
 [9,0,7,8,2,0,6,3,4],
 [5,8,6,0,7,4,1,2,9],
@@ -14,42 +15,36 @@ board = [
 # s4 s5 s6
 # s7 s8 s9
 
-s1 = []
-s2 = []
-s3 = []
-s4 =[]
-s5 = []
-s6 = []
-s7 = []
-s8 = []
-s9 = []
-#def readboard(board):
+def get_section(board, section):
+    a = dict([
+    ('s1' , board[0][0:3] + board[1][0:3] + board[2][0:3]),
+    ('s4' , board[3][0:3] + board[4][0:3] + board[5][0:3]),
+    ('s7' , board[6][0:3] + board[7][0:3] + board[8][0:3]),
 
-def initboard(board):
-    s1 = board[0][0:3] + board[1][0:3] + board[2][0:3]
-    s4 = board[3][0:3] + board[4][0:3] + board[5][0:3]
-    s7 = board[6][0:3] + board[7][0:3] + board[8][0:3]
+    ('s2' , board[0][3:6] + board[1][3:6] + board[2][3:6]),
+    ('s5' , board[3][3:6] + board[4][3:6] + board[5][3:6]),
+    ('s8' , board[6][3:6] + board[7][3:6] + board[8][3:6]),
 
-    s2 = board[0][3:6] + board[1][3:6] + board[2][3:6]
-    s5 = board[3][3:6] + board[4][3:6] + board[5][3:6]
-    s8 = board[6][3:6] + board[7][3:6] + board[8][3:6]
+    ('s3' , board[0][6:] + board[1][6:] + board[2][6:]),
+    ('s6' , board[3][6:] + board[4][6:] + board[5][6:]),
+    ('s9' , board[6][6:] + board[7][6:] + board[8][6:])
+    ])
+    return a[section]
 
-    s3 = board[0][6:] + board[1][6:] + board[2][6:]
-    s6 = board[3][6:] + board[4][6:] + board[5][6:]
-    s9 = board[6][6:] + board[7][6:] + board[8][6:]
+def get_row(board, row):
+    return board[row]
 
+def get_col(board, col):
+    #bad method, should be better one with for loop
+    return [board[0][col], board[1][col],board[2][col],board[3][col],board[4][col],board[5][col],board[6][col],board[7][col],board[8][col]]
 
 def printboard(board):
-    print s1,s2,s3
-    print s4,s5,s6
-    print s7,s8,s9
-'''
     for i in range(len(board)):
         for j in range(len(board[0])):
                 print board[i][j],
 
         print "\n"
-'''
+
 
 def get_sectionums(section):
     retarr = []
@@ -83,5 +78,5 @@ def solved(section):
             solved = False
     return solved
 
-
-printboard(board)
+#printboard(board)
+#print s1
